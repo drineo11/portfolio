0cc28187707e0611950406a6a32c46399896ff4c
drineo <drineo@hotmail.com>
2023-07-13 18:06:23 -0300

    Ajustes e correções

--- portifolio/src/components/Contacts/Contacts.tsx
+++ portifolio/src/components/Contacts/Contacts.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./sass/Contacts.scss";
 
 import ContactsIcon from "../../assets/images/contactsIcon.png";
